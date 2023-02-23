75bbdd52b85965c7a3c6ec3c6e12de201c38dfd3
李云飞 <liyunfei02@daojia-inc.com>
2023-02-23 18:03:35 +0800

    feat: post文章

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UsersModule } from './users/users.module';
       synchronize: true,
     }),
     UsersModule,
+    PostsModule,
   ],
 })
 export class AppModule {}
