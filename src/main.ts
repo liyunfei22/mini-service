fcba0e96ed7087a5b175386f61a02533f92f084f
李云飞 <liyunfei02@daojia-inc.com>
2023-02-23 15:46:56 +0800

    feat: users add typeORM

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api'); // 设置全局路由前缀
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
