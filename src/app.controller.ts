f160a134f07d517f6528d62c3f37891c2a0bf2e4
李云飞 <liyunfei02@daojia-inc.com>
2023-03-10 12:50:02 +0800

    feat:  空白项目

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,56 +1,12 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  BadRequestException,
-  UseGuards,
-  Req,
-} from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
-import { CreateUserDto } from 'src/user/dto/create-user.dto';
-import { ConfigService } from '@nestjs/config';
-import { AuthGuard } from '@nestjs/passport';
-import { AuthService } from 'src/auth/auth.service';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly userService: UserService,
-    private readonly authService: AuthService,
-    private configService: ConfigService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
-  @UseGuards(AuthGuard('local'))
-  @Post('login')
-  async login(
-    @Body() createUserDto: CreateUserDto,
-  ): Promise<{ token: string }> {
-    console.log(this.configService.get<string>('port'));
-    const { phone, password } = createUserDto;
-    const token = await this.authService.login({ phone, password });
-    return token;
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
-
-  @Post('/register')
-  async register(
-    @Body() createUserDto: CreateUserDto,
-  ): Promise<{ id: number }> {
-    const { phone, password } = createUserDto;
-    const existingUser = await this.userService.findOne(phone);
-    console.log(existingUser);
-    if (existingUser) {
-      throw new BadRequestException('手机号已被注册');
-    }
-    const user = await this.userService.create(phone, password);
-    console.log(user);
-    return { id: user.id };
-  }
-
-  @UseGuards(AuthGuard('jwt'))
-  @Get('login')
-  getUserInfo(@Req() req) {
-    return req.user;
-  }
-
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,42 +1,11 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BeefModule } from './beef/beef.module';
-import { FeedModule } from './feed/feed.module';
-import { UserModule } from './user/user.module';
-import { LoggerMiddleware } from './middleware/logger.middleware';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import configuration from './config/configuration';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { OrderModule } from './order/order.module';
-
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '12345678',
-      database: 'db_min_cal',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    BeefModule,
-    FeedModule,
-    UserModule,
-    AuthModule,
-    OrderModule,
-  ],
+  imports: [CatsModule, UserModule],
   controllers: [AppController],
-  providers: [],
+  providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
